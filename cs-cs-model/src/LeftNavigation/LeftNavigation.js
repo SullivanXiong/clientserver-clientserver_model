83ed7a100be39074add537e1d9cc7d22b871e98e
sxiong5307 <sxiong5307@gmail.com>
2021-12-09 20:59:55 -0800

    cscs

--- cs-cs-model/src/LeftNavigation/LeftNavigation.js
+++ cs-cs-model/src/LeftNavigation/LeftNavigation.js
@@ -6,7 +6,7 @@ export default function leftNavigation({ setNavState, navigations }) {
     console.log(navigations.length);
     return (
         <div className="leftNavWrapper">
-            {navigations.map(navigation => <NavButton style={{height: `calc((100vh/${navigations.length}) - 2px)`, width:'calc(20vw - 2px)', border: 'solid white 1px', 'verticalAlign': 'middle', 'lineHeight': `calc((100vh/${navigations.length}) - 2px)`, 'textAlign': 'center'}} name={navigation.name} onClick={() => { setNavState(navigation.redirect) }}/> )}
+            {navigations.map(navigation => <NavButton style={{border: 'solid white 1px', 'verticalAlign': 'middle', 'textAlign': 'center'}} name={navigation.name} onClick={() => { setNavState(navigation.redirect) }}/> )}
         </div>
     )
 }
--- cs-cs-model/src/rightNav/Port/Port.js
+++ cs-cs-model/src/rightNav/Port/Port.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Port.css';
 
 export default function Port() {
+
+
     return (
-        <div>
-            Port
+        <div style={{width: "60vw", height: "100vh"}}>
+            <svg
+                style={{width: "100%", height: "100%"}}>
+                <circle
+                    cx="50%"
+                    cy="50%"
+                    r="20"
+                    style={{stroke: 'none', fill: '#000000'}}
+                />
+                <circle
+                    cx="calc(50% + 20px)"
+                    cy="calc(50% + 20px)"
+                    r="20"
+                    style={{stroke: 'none', fill: '#000000'}}
+                />
+            </svg>
         </div>
     )
 }
